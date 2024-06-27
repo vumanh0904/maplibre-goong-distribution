0ce395095b79f0f07ac57e2bf8d4578a727b4cc0
imap Mac <goongimap@icloud.com>
2024-06-27 14:17:28 +0700

    thay đổi url download

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MapLibre",
-            url: "https://github.com/vumanh0904/MapLibre/releases/tag/1.0.2/MapLibre.dynamic.xcframework.zip",
+            url: "https://github.com/vumanh0904/MapLibre/releases/download/1.0.2/MapLibre.dynamic.xcframework.zip",
             checksum: "09bcfe6f2684193f07dc6213b880e4cd0e2fa4e2031a62123c6b5f519f49e0b1")
     ]
 )
