e815c55ee2aada8cc0bbec999b5f474578547d30
imap Mac <goongimap@icloud.com>
2024-06-27 14:23:50 +0700

    thay đổi tên

--- Package.swift
+++ Package.swift
@@ -2,17 +2,17 @@
 import PackageDescription
 
 let package = Package(
-    name: "MapLibre Native",
+    name: "GoongMap",
     products: [
         .library(
-            name: "MapLibre",
-            targets: ["MapLibre"])
+            name: "GoongMap",
+            targets: ["GoongMap"])
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
-            name: "MapLibre",
+            name: "GoongMap",
             url: "https://github.com/vumanh0904/MapLibre/releases/download/1.0.2/MapLibre.dynamic.xcframework.zip",
             checksum: "09bcfe6f2684193f07dc6213b880e4cd0e2fa4e2031a62123c6b5f519f49e0b1")
     ]
